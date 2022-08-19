046fdb063e8737e9ca32c592ad21fba7ae2861b1
Jeon, Jae Byeong <royoet@yonsei.ac.kr>
2022-08-20 01:36:34 +0900

    asdf

--- purple/purple/settings.py
+++ purple/purple/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'staticfiles')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 STATIC_ROOT = os.path.join('staticfiles')
