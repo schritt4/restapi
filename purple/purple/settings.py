1ad3891348f0acf6fd44bac27cdb3ba10cb8c146
Jeon, Jae Byeong <royoet@yonsei.ac.kr>
2022-08-20 00:50:36 +0900

    d

--- purple/purple/settings.py
+++ purple/purple/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = ('https://127.0.0.1:8000', 'http://localhost:3000')
+CORS_ORIGIN_WHITELIST = ('https://127.0.0.1:3000', 'http://localhost:3000')
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'purple.urls'
