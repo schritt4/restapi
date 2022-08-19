c6443bc46aad705af0108b0939c8b4d5f7c591fb
Jeon, Jae Byeong <royoet@yonsei.ac.kr>
2022-08-20 01:42:51 +0900

    asdf

--- purple/purple/settings.py
+++ purple/purple/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
 ]
 
-STATIC_ROOT = os.path.join('staticfiles')
+STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
